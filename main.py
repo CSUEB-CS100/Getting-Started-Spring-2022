8f4cd32e7d72885e7d1c154f7a3f41c475876a6b
Paul Raupach <paul@raupach.com>
2022-02-02 13:17:18 -0800

    Getting started Code from class

---
+++ main.py
@@ -0,0 +1,67 @@
+print("Hello World!")
+print('Hello', "Bob?")
+print("Hello", "Jim", 9, 10)
+
+print(5 + 3)
+print(5 - 3)
+print(5 * 3)
+print(5 * 3 * 6)
+print(5 / 3)
+print(5 // 3)
+print(5 % 3)
+print(5 ** 3 + 5)
+print(5 ** (3 + 5))
+
+print(5 > 3)
+print(5 < 3)
+print(5 >= 3)
+print(5 <= 3)
+print(5 == 3)
+print(5 != 3)
+
+print(5.6 + 8.7)
+
+print(.1 + .1 + .1 == .3) #falsish
+print(.1 + .1 + .1)
+
+bobsAge = 32
+print(bobsAge)
+
+bobsName = "Bobby"
+
+print(bobsName)
+print(bobsName, "is", bobsAge, "years old.")
+
+bobsAge = 87
+print(bobsName, "is", bobsAge, "years old.")
+
+
+bobsAge = "twenty-five"
+print(bobsName, "is", bobsAge, "years old.")
+
+a = 34
+b = 56
+c = a + b
+print(a,b,c)
+
+print(type(67))
+print(type("Jane"))
+print(type(6.90))
+
+print(type(a))
+
+isGreater = (5 == 4)
+print(isGreater)
+
+userInput = input("Give me a number: ")
+print("You entered the number: ", userInput)
+
+userInput = int(userInput)
+newNumber = userInput + 2
+print("Your number plus 2 is", newNumber)
+
+
+
+
+
+
